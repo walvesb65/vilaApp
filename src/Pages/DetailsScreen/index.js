8d98ccedebfda1814e2110f62219d1c6237eec48
Willian Alves <willian.alvesb65@gmail.com>
2022-03-07 10:35:18 -0300

    Fix: Import task component in details page

--- src/Pages/DetailsScreen/index.js
+++ src/Pages/DetailsScreen/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
+import Tasks from '../Tasks';
 
 export default function DetailsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Tasks />
       <Text>Details Screen</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.push('Details')}
       />
     </View>
   );
